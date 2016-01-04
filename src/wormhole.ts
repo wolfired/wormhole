f7543e9cac465008bdc01288de0b2772304b8485
LinkWu <wjianl@qq.com>
2016-01-05 00:53:15 +0800

    实现启动类

--- src/wormhole.ts
+++ src/wormhole.ts
@@ -1,10 +1,38 @@
-/// <reference path="signal/SignalRouter.ts" />
+class Wormhole {
+	public setup(w:number, h:number):Wormhole{
+		this._canvas = <HTMLCanvasElement> document.createElement("canvas");
+		this._canvas.width = w;
+		this._canvas.height = h;
+		
+		document.body.appendChild(this._canvas);
+		
+		this._context = <WebGLRenderingContext> this._canvas.getContext("webgl");
+		this._context.clearColor(0.9, 0.9, 0.9, 1.0);
+		
+		return this;
+	}
+	
+	public start():void{
+		var loop = (dt:number):void=>{
+			this._context.clear(this._context.COLOR_BUFFER_BIT);
 
-import sig = wormhole.signal;
+			
+			this._handle = window.requestAnimationFrame(loop);
+		};
+		
+		loop(0.0);
+	}
+	
+	public stop():void{
+		window.cancelAnimationFrame(this._handle);
+	}
+	
+	private _handle:number;
+	
+	private _canvas:HTMLCanvasElement;
+	private _context:WebGLRenderingContext;
+}
 
 
-
-
-var sr = new sig.SignalRouter();
-sr.addHandler(1000, (s:sig.Signal):void => { console.log(this) });
-sr.route(new sig.Signal(1000));
+var wh:Wormhole = new Wormhole();
+wh.setup(500, 500).start();
