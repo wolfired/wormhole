dc7588fe71743ff1c6e8c6102ce1e46c38f42af3
LinkWu <wjianl@qq.com>
2016-02-28 19:56:34 +0800

    修正代码错误

--- src/ui/VisualElement.ts
+++ src/ui/VisualElement.ts
@@ -1,15 +1,28 @@
-/// <reference path="../component/Entity.ts" />
 /// <reference path="../component/Component4Transform.ts" />
+/// <reference path="../component/Entity.ts" />
+/// <reference path="../signal/ISignalRouter.ts" />
 
 import comp = wormhole.component;
+import sig = wormhole.signal;
 
-		export class VisualElement extends comp.Entity {
 namespace wormhole {
 	export namespace ui {
+		export class VisualElement extends comp.Entity implements sig.ISignalRouter {
 			public constructor(){
 				super();
 				
 				this.addComponent(comp.Component4Transform.ID);
+			}
+			
+			public addHandler(sid:sig.SignalID, handler:sig.SignalHandler):void{
+				
+			}
+			
+			public delHandler(sid:sig.SignalID, handler:sig.SignalHandler):void{
+				
+			}
+			public route(s:sig.Signal):void{
+				
 			}
 		}
 	}
